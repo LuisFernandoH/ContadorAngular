b627f502e19322c8310139356c82e4ce14d0dd21
LuisFernandoH <luisspartan10@gmail.com>
2024-03-21 09:57:53 -0600

    Primer commit de deberitas

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
